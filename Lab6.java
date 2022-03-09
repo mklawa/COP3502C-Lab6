4cac783fd62e6f0f251f139e32f6cad47e8e9c49
Mary Klawa <100811655+mklawa@users.noreply.github.com>
2022-03-09 14:12:25 -0500

    changed + to -

--- Lab6.java
+++ Lab6.java
@@ -2,6 +2,6 @@ public class Lab6 {
     public static void main(String[] args) {
         int num = 6;
         int nextNum = 4;
-        int newNum = num + nextNum;
+        int newNum = num - nextNum;
     }
 }
